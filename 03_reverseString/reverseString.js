322ad98ae3313d939cd1979f1dc2a4c7428455e8
YoYoDough <yosuf818s@gmail.com>
2022-05-24 21:03:25 -0700

    Made the function to get the reverse of a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    var reverse = ""
+    for (i = 0; i < string.length; i++)
+    {
+        var char = string.charAt(i)
+        reverse = char + reverse;
+    }
+    return reverse;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
